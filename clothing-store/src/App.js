d9eefe1213e4c69a72eee636bf67a79fc6d49d63
quantumomid <omid.wakili@outlook.com>
2021-10-24 19:57:03 +0100

    adding Header component to hold the Link counterparts to the Route from react router dom and appplying some styling

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/HomePage'
 import Shop from './pages/shop/Shop'
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
