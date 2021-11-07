b9bc3bf686e0266b9b0aa5a823d664a6e9548fed
quantumomid <omid.wakili@outlook.com>
2021-11-07 16:06:23 +0000

    use mastatetoprops in cartIcon to determine the total cart items using .reduce()

--- clothing-store/src/components/cart-icon/CartIcon.js
+++ clothing-store/src/components/cart-icon/CartIcon.js
@@ -4,17 +4,21 @@ import { toggleCartHidden } from "../../redux/cart/cartActions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./CartIcon.scss";
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     )
 }
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity+cartItem.quantity, 0)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
