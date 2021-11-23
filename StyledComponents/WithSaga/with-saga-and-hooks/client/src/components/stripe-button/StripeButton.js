af188ed23bf43c44795a29386ef5ae0671ffb938
quantumomid <omid.wakili@outlook.com>
2021-11-23 14:22:23 +0000

    add backend folder and server to handle stripe payments and start new context api implementation

--- StyledComponents/WithSaga/with-saga-and-hooks/client/src/components/stripe-button/StripeButton.js
+++ StyledComponents/WithSaga/with-saga-and-hooks/client/src/components/stripe-button/StripeButton.js
@@ -1,5 +1,6 @@
 import StripeCheckout from "react-stripe-checkout";
 import crownLogo from "../../assets/crown.svg";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
     //Stripe needs value to be provided in smallest units of the currency i..e., pence for british pounds 
@@ -8,8 +9,21 @@ const StripeButton = ({ price }) => {
     const publishableKey = "pk_test_51JxR6iI31TOr1s6tKQVipqKunoTNMFXw92raFPZjKMxOYKAdgvZJUVNK6Rdy9nj7U7S0w3OsoGoiTAzwTswTrlJN00nsGQe3lV";
 
     const handleToken = (token) => {
-        console.log(token);
-        alert("Payment Successful");
+        // console.log(token);
+        // console.alert("Payment Successful");
+        axios({ 
+            url: "payment",
+            method: "post",
+            data: {
+                amount: priceForStripe,
+                token
+            }
+         }).then(response => {
+             alert("Payment successful")
+         }).catch(error => {
+             console.log("Payment error: ", JSON.parse(error))
+             alert("There was an issue with your payment. Please make sure you use the provided test credit card.")
+         })
     }
 
     return(
@@ -20,6 +34,7 @@ const StripeButton = ({ price }) => {
             shippingAddress
             image={crownLogo}
             description={`Your total is £${price}`}
+            currency="GBP"
             amount={priceForStripe}
             panelLabel="Pay Now"
             token={handleToken}
---
+++ StyledComponents/WithSaga/with-saga-and-hooks/server.js
@@ -0,0 +1,56 @@
+// this is older style importing before ES6 imports
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+if(process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+// Instantiate a new express application
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Tells our server to process any incoming requests by converting their
+// body tag to JSON so that we can use it
+app.use(bodyParser.json());
+// Checks if the urls are in acceptable format
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if(process.env.NODE_ENV === "production"){
+    // serve all the static files in our build
+    // __dirname tells us what directory we are currently in
+    app.use(express.static(path.join(__dirname, "client/build")));
+    // If the user hits ANY route do the following for the response 
+    // req is the request and res is the response we sent back
+    //  i.e. we send the index.html file which contains the entry 
+    // point of our REACT application
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "client/build", "index.html"))
+    });
+}
+
+// listen on port 
+app.listen(port, error => {
+    // throw error if any error comes up
+    if(error) throw error;
+    // otherwise just log out port
+    console.log("Server is running on port: " + port);
+});
+
+app.post("/payment", (req, res) => {
+    
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: "gbp"
+    };
+
+    stripe.charges.create(body, (stripeError, stripeResponse) => {
+        if(stripeError){
+            res.status(500).send({ error: stripeError });
+        } else{
+            res.status(200).send({ success: stripeResponse });
+        }
+    });
+})
