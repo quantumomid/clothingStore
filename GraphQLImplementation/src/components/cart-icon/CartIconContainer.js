13edce3bbf0b5e53b380218f8317e026e73a6c16
quantumomid <omid.wakili@outlook.com>
2021-11-24 18:01:42 +0000

    redo cart icon graphql apollo implementation to use HOC pattern with graphql() and flowRight() methods - before used compose() but depreacated

--- GraphQLImplementation/src/components/cart-icon/CartIconContainer.js
+++ GraphQLImplementation/src/components/cart-icon/CartIconContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Mutation, Query } from "react-apollo";
+import { graphql } from "react-apollo";
+import { flowRight } from 'lodash';
 import { gql } from "apollo-boost";
 import CartIcon from "./cart-icon.component";
 
@@ -16,19 +17,32 @@ const getItemCount = gql`
     }
 `
 
-const CartIconContainer = () => (
-    <Query query={getItemCount}>
-        {
-            ({ data }) => {
-                const { itemCount } = data;
-                return (
-                    <Mutation mutation={toggleCartHidden}> 
-                        { toggleCartHidden => <CartIcon itemCount={itemCount} toggleCartHidden={toggleCartHidden} /> }
-                    </Mutation>
-                )
-            }
-        }
-    </Query>
-)
+const CartIconContainer = ({ data: { itemCount }, toggleCartHidden }) => {
+    // console.log(props);
+    // const { itemCount } = data;
+    return(
+        <CartIcon itemCount={itemCount} toggleCartHidden={toggleCartHidden} /> 
+    )
+}
 
-export default CartIconContainer;
+// OLD way (Actually better because with newer/current Apollo versions - move to move away from HOC pattern below)
+// const CartIconContainer = () => (
+//     <Query query={getItemCount}>
+//         {
+//             ({ data }) => {
+//                 const { itemCount } = data;
+//                 return (
+//                     <Mutation mutation={toggleCartHidden}> 
+//                         { toggleCartHidden => <CartIcon itemCount={itemCount} toggleCartHidden={toggleCartHidden} /> }
+//                     </Mutation>
+//                 )
+//             }
+//         }
+//     </Query>
+// )
+
+// HOC Pattern Method
+export default flowRight(
+    graphql(getItemCount),
+    graphql(toggleCartHidden, { name: "toggleCartHidden" })
+)(CartIconContainer);
